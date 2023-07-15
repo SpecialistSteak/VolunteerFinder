bc54eaf9ecec75469f663c9d6162e3a6e028ef19
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-07-15 12:11:05 -0400

    Volunteer.java created

--- src/main/java/org/drivenbysteak/VolunteerFinder/Objects/Volunteer.java
+++ src/main/java/org/drivenbysteak/VolunteerFinder/Objects/Volunteer.java
@@ -3,8 +3,6 @@ package org.drivenbysteak.VolunteerFinder.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.net.URL;
-
 @Data
 @AllArgsConstructor
 public class Volunteer {
