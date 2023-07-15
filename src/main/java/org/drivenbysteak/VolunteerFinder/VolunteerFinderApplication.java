4d1d749093e80667d89e05aded8efa4b67f366cb
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-07-15 11:56:40 -0400

    Initial commit

---
+++ src/main/java/org/drivenbysteak/VolunteerFinder/VolunteerFinderApplication.java
@@ -0,0 +1,13 @@
+package org.drivenbysteak.VolunteerFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/drivenbysteak/VolunteerFinder/VolunteerFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.drivenbysteak.VolunteerFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
